b1a01e82c934ff367a53784045b2a5ae8f4f04ec
40718533 <40718533@qq.com>
2019-06-24 22:01:17 +0800

    1

---
+++ AdminTemplate/Controllers/LatitudeDetailTwoController.cs
@@ -0,0 +1,45 @@
+﻿using AdminTemplate.service.Dto.LatitudeDetail;
+using AdminTemplate.service.Services;
+using GlobalConfiguration.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminTemplate.Controllers
+{
+    [Route("api/[controller]")]
+    public class LatitudeDetailTwoController : ControllerBase
+    {
+        private readonly LatitudeDetailTwoService _service;
+
+        public LatitudeDetailTwoController(LatitudeDetailTwoService service)
+        {
+            _service = service;
+        }
+        [HttpPost, Route("Add")]
+        public NetResult Add([FromBody]LatitudeDetailDto model)
+        {
+
+
+            return _service.Add(model);
+        }
+        [HttpPost, Route("Delete/{id}")]
+        public NetResult Delete(string id)
+        {
+            return _service.Delete(id);
+        }
+        [HttpPost, Route("Update")]
+        public NetResult Update([FromBody]LatitudeDetailDto model)
+        {
+            return _service.Update(model);
+        }
+        [HttpGet, Route("List")]
+        public NetResult List(string id, PaginationStartAndLengthFilter filter)
+        {
+            return _service.List(id, filter);
+        }
+        [HttpGet, Route("GetPicker")]
+        public NetResult GetPicker()
+        {
+            return _service.GetPicker();
+        }
+    }
+}
