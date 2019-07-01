2a7710f085c98cc9c346e563f35cb5b8dae999f9
407185033 <407185033@qq.com>
2019-07-01 11:06:07 +0800

    1

--- AdminTemplate.service/Services/QtDetailService.cs
+++ AdminTemplate.service/Services/QtDetailService.cs
@@ -9,8 +9,13 @@ namespace AdminTemplate.service.Services
     {
         public NetResult Get(string id)
         {
-            var model = DbContext.QtDetail.Include(o => o.QtDetailItem).FirstOrDefault(p => p.Id.Equals(id));
-            return ResponseBodyEntity(model);
+            var model = DbContext.QtDetail.AsNoTracking().Include(o => o.QtDetailItem).FirstOrDefault(p => p.Id.Equals(id));
+            if (model != null)
+            {
+                model.QtDetailItem = model.QtDetailItem.OrderBy(o => o.Order).ToList();
+                return ResponseBodyEntity(model);
+            }
+            return ResponseBodyEntity("", EnumResult.Error, "对象不存在");
         }
     }
 }
